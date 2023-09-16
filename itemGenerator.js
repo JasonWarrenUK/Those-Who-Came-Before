f016f63cbf126c09946c465bf9a9007e7e6c772e
Jason Warren <contact.jwarren@gmail.com>
2023-09-16 23:17:09 +0100

    Continued work on item generator
    Organised code for readability
    
    Created function to generate multiple item types without repetition
    
    Created materials for items to be constructed from, organised by categories (e.g. bone contains horn, ivory etc)
    
    Materials are now applied to items on creation, so flint pot instead of pot

--- itemGenerator.js
+++ itemGenerator.js
@@ -1,37 +1,69 @@
-const items = ["axe", "pot", "spear"];
-let itemsAdvailable = items.slice();
-let itemsUsed = [];
+// ENCYCLOPEDIA
+  // ITEMS
+    const items = [
+      {type: "axe"},
+      {type: "pot"},
+      {type: "spear"}
+    ];
 
-function indexRandom(arr) {
-  let max = arr.length - 1;
-  let index = Math.floor(Math.random() * (max + 1));
-  return index;
-}
+  //MATERIALS
+    const materials = [bone, metal, stone, wood];
+    const bone = ["bone", "horn", "ivory"];
+    const metal = ["bronze", "copper", "gold", "iron", "mercury", "pewter", "silver", "steel"];
+    const stone = ["flint", "granite", "obsidian"];
+    const wood = ["ash", "elm", "mahogany", "oak", "willow"];
 
-function useItemNamed(item) {
-  let result = "Item Type ";
+  //THIS GAME
+    let itemsAvailable = items.slice();
+    let itemsUsed = [];
 
-  if (!items.includes(item)) {
-    result += "Doesn't Exist";
-  } else if (itemsUsed.includes(item)) {
-    result += "Already Used";
-  } else {
-    itemsUsed.push(item);
-    //remove from itemsAvailable
-    result += "Created";
+// ITEM CREATION
+  // BASIC
+    function itemCreateNamed(item) {
+      let feedback = "Item Type ";
+      if (!items.includes(item)) {
+        feedback += "Doesn't Exist";
+      } else if (itemsUsed.includes(item)) {
+        feedback += "Already Used";
+      } else {
+        itemsUsed.push(item);
+        itemsAvailable.splice(indexOf(item), 1);
+        feedback += "Created";
+      }
+      console.log(result);
+      console.log("Items In Use: " + itemsUsed);
+      console.log("Unused Items: " + itemsAvailable);
+    }
+
+    function itemCreateRandom() {
+      let item = itemsAvailable[indexRandom(itemsAvailable)];
+
+      itemsUsed.push(item);
+      itemsAvailable.splice(indexOf(item), 1);
+      
+      itemTraitsApply(item);
+
+      let feedback = "Item Type " + item.material + " " + item.type + " Created";
+
+      console.log(feedback);
+      console.log("Items In Use: " + itemsUsed);
+      console.log("Unused Items: " + itemsAvailable);
+    }
+
+    function itemCreateSet(amount) {
+      for (let i = 0; i <= amount; i++) {
+        itemCreateRandom();
+      }
+    }
+
+    function itemTraitsApply(item) {
+      let materialCategory = materials[indexRandom(materials)];
+      item.material = materialCategory[indexRandom(materialCategory)];
+    }
+
+// UTILITIES
+  function indexRandom(arr) {
+    let max = arr.length - 1;
+    let index = Math.floor(Math.random() * (max + 1));
+    return index;
   }
-
-  console.log(result);
-  console.log("Items In Use: " + itemsUsed);
-  console.log("Unused Items: " + itemsAvailable);
-}
-
-function useItemRandom() {
-  let item = itemsAvailable[indexRandom(itemsAvailable)];
-  
-  itemsUsed.push(item);
-  //remove it from itemsAvailable;
-  
-  console.log("Items In Use: " + itemsUsed);
-  console.log("Unused Items: " + itemsAvailable);
-}
