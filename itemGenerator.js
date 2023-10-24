8a61f892a2b4615e7d0ea9f6cfbf09db935b449e
Jason Warren <contact.jwarren@gmail.com>
2023-10-24 21:27:19 +0100

    Working Item Generator

--- itemGenerator.js
+++ itemGenerator.js
@@ -2,16 +2,23 @@
   // ITEMS
     const items = [
       {type: "axe"},
+      {type: "brooch"},
+      {type: "dagger"},
+      {type: "drinking vessel"},
+      {type: "pendant"},
       {type: "pot"},
-      {type: "spear"}
+      {type: "spear"},
+      {type: "sword"},
+      {type: "urn"}
     ];
 
   //MATERIALS
-    const materials = [bone, metal, stone, wood];
     const bone = ["bone", "horn", "ivory"];
     const metal = ["bronze", "copper", "gold", "iron", "mercury", "pewter", "silver", "steel"];
     const stone = ["flint", "granite", "obsidian"];
     const wood = ["ash", "elm", "mahogany", "oak", "willow"];
+
+    const materials = [bone, metal, stone, wood];
 
   //THIS GAME
     let itemsAvailable = items.slice();
@@ -20,6 +27,8 @@
 // ITEM CREATION
   // BASIC
     function itemCreateNamed(item) {
+      console.log("Running itemCreateNames");
+
       let feedback = "Item Type ";
       if (!items.includes(item)) {
         feedback += "Doesn't Exist";
@@ -36,29 +45,42 @@
     }
 
     function itemCreateRandom() {
+      console.log("Running itemCreateRandom");
       let item = itemsAvailable[indexRandom(itemsAvailable)];
 
-      itemsUsed.push(item);
-      itemsAvailable.splice(indexOf(item), 1);
+      itemsUsed.push(item.type);
+      itemsAvailable.splice(itemsAvailable.indexOf(item), 1);
       
       itemTraitsApply(item);
 
-      let feedback = "Item Type " + item.material + " " + item.type + " Created";
+      let feedback = "Item Created: " + item.material + " " + item.type;
 
       console.log(feedback);
-      console.log("Items In Use: " + itemsUsed);
-      console.log("Unused Items: " + itemsAvailable);
+      console.log("itemsUsed: " + itemsUsed);
+      console.log("itemsAvailable: " + itemsAvailable);
+
+      item = item.material + " " + item.type;
+      
+      return item;
     }
 
     function itemCreateSet(amount) {
+      console.log("Running itemCreateSet");
+      let itemSet = [];
+      
       for (let i = 0; i <= amount; i++) {
-        itemCreateRandom();
+        itemSet.push(itemCreateRandom());
       }
+
+      return itemSet;
     }
 
     function itemTraitsApply(item) {
+      console.log("Running itemTraitsApply");
       let materialCategory = materials[indexRandom(materials)];
       item.material = materialCategory[indexRandom(materialCategory)];
+
+      return item;
     }
 
 // UTILITIES
@@ -66,4 +88,15 @@
     let max = arr.length - 1;
     let index = Math.floor(Math.random() * (max + 1));
     return index;
+  }
+
+  // PAGE
+  let genButton = document.getElementById("genButton");
+  let contentBucket = document.getElementById("holder");
+  
+  genButton.addEventListener("click", pushToPage(itemCreateSet(5)));
+
+  function pushToPage(input) {
+    console.log("Running pushToPage ");
+    holder.innerHTML = "<ul><li>" + input.join("</li><li>") + "</li></ul>";
   }
