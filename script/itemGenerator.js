f5dd0ac4bf0bc50bf2013c070ba1569dc9193bcc
Jason Warren <contact.jwarren@gmail.com>
2023-11-12 00:06:17 +0000

    Dark Mode Item Generator

--- script/itemGenerator.js
+++ script/itemGenerator.js
@@ -1,102 +1,111 @@
+// UTILITIES
+function indexRandom(arr) {
+  let max = arr.length - 1;
+  let index = Math.floor(Math.random() * (max + 1));
+  return index;
+}
+
 // ENCYCLOPEDIA
   // ITEMS
-    const items = [
-      {type: "axe"},
-      {type: "brooch"},
-      {type: "dagger"},
-      {type: "drinking vessel"},
-      {type: "pendant"},
-      {type: "pot"},
-      {type: "spear"},
-      {type: "sword"},
-      {type: "urn"}
-    ];
+  const items = [
+    {type: "axe"},
+    {type: "brooch"},
+    {type: "dagger"},
+    {type: "drinking vessel"},
+    {type: "pendant"},
+    {type: "pot"},
+    {type: "spear"},
+    {type: "sword"},
+    {type: "urn"}
+  ];
 
   //MATERIALS
-    const materials = [bone, metal, stone, wood];
-    
-    const bone = ["bone", "horn", "ivory"];
-    const metal = ["bronze", "copper", "gold", "iron", "mercury", "pewter", "silver", "steel"];
-    const stone = ["flint", "granite", "obsidian"];
-    const wood = ["ash", "elm", "mahogany", "oak", "willow"];
+  const bone = ["bone", "horn", "ivory"];
+  const metal = ["bronze", "copper", "gold", "iron", "mercury", "pewter", "silver", "steel"];
+  const stone = ["flint", "granite", "obsidian"];
+  const wood = ["ash", "elm", "mahogany", "oak", "willow"];
+  const materials = [bone, metal, stone, wood];
 
   //THIS GAME
-    let itemsAvailable = items.slice();
-    let itemsUsed = [];
+  let itemsAvailable = items.slice();
+  let itemsUsed = [];
 
 // ITEM CREATION
-  // BASIC
-    function itemCreateNamed(item) {
-      console.log("Running itemCreateNames");
+function itemCreateNamed(item) {
+  console.log("Running itemCreateNames");
 
-      let feedback = "Item Type ";
-      if (!items.includes(item)) {
-        feedback += "Doesn't Exist";
-      } else if (itemsUsed.includes(item)) {
-        feedback += "Already Used";
-      } else {
-        itemsUsed.push(item);
-        itemsAvailable.splice(indexOf(item), 1);
-        feedback += "Created";
-      }
-      console.log(result);
-      console.log("Items In Use: " + itemsUsed);
-      console.log("Unused Items: " + itemsAvailable);
-    }
-
-    function itemCreateRandom() {
-      console.log("Running itemCreateRandom");
-      let item = itemsAvailable[indexRandom(itemsAvailable)];
-
-      itemsUsed.push(item.type);
-      itemsAvailable.splice(itemsAvailable.indexOf(item), 1);
-      
-      itemTraitsApply(item);
-
-      let feedback = "Item Created: " + item.material + " " + item.type;
-
-      console.log(feedback);
-      console.log("itemsUsed: " + itemsUsed);
-      console.log("itemsAvailable: " + itemsAvailable);
-
-      item = item.material + " " + item.type;
-      
-      return item;
-    }
-
-    function itemCreateSet(amount) {
-      console.log("Running itemCreateSet");
-      let itemSet = [];
-      
-      for (let i = 0; i <= amount; i++) {
-        itemSet.push(itemCreateRandom());
-      }
-
-      return itemSet;
-    }
-
-    function itemTraitsApply(item) {
-      console.log("Running itemTraitsApply");
-      let materialCategory = materials[indexRandom(materials)];
-      item.material = materialCategory[indexRandom(materialCategory)];
-
-      return item;
-    }
-
-// UTILITIES
-  function indexRandom(arr) {
-    let max = arr.length - 1;
-    let index = Math.floor(Math.random() * (max + 1));
-    return index;
+  let feedback = "Item Type ";
+  if (!items.includes(item)) {
+    feedback += "Doesn't Exist";
+  } else if (itemsUsed.includes(item)) {
+    feedback += "Already Used";
+  } else {
+    // itemsUsed.push(item);
+    // itemsAvailable.splice(itemsAvailable.indexOf(item), 1);
+    feedback += "Created";
   }
+  console.log(feedback);
+  console.log("Items In Use: " + itemsUsed);
+  console.log("Unused Items: " + itemsAvailable);
+}
 
-  // PAGE
-  let genButton = document.getElementById("genButton");
-  let contentBucket = document.getElementById("holder");
+function itemCreateRandom() {
+  /* Now, let's dance with the itemCreateRandom function. You're trying to access item.material and item.type, but you never define these properties in your items. You might want to consider adding them to your item objects in the items array. */
+
+  console.log("Running itemCreateRandom");
+  let item = itemsAvailable[indexRandom(itemsAvailable)];
+
+  // itemsUsed.push(item.type);
+  // itemsAvailable.splice(itemsAvailable.indexOf(item), 1);
   
-  genButton.addEventListener("click", pushToPage(itemCreateSet(5)));
+  itemTraitsApply(item);
 
-  function pushToPage(input) {
-    console.log("Running pushToPage ");
-    holder.innerHTML = "<ul><li>" + input.join("</li><li>") + "</li></ul>";
+  let feedback = "Item Created: " + item.material + " " + item.type;
+
+  console.log(feedback);
+  console.log("itemsUsed: " + itemsUsed);
+  console.log("itemsAvailable: " + itemsAvailable);
+
+  item = item.material + " " + item.type;
+  
+  return item;
+}
+
+function itemCreateSet(amount) {
+  console.log("Running itemCreateSet");
+  let itemSet = [];
+  
+  for (let i = 0; i < amount; i++) {
+    itemSet.push(itemCreateRandom());
   }
+
+  return itemSet;
+}
+
+function itemTraitsApply(item) {
+  console.log("Running itemTraitsApply");
+  let materialCategory = materials[indexRandom(materials)];
+  item.material = materialCategory[indexRandom(materialCategory)];
+
+  return item;
+}
+
+
+// WEB DISPLAY
+function pushToGeneratorOutput(input) {
+  console.group("pushToGeneratorOutput");
+    console.log("Clearing Div")
+    generatorOutput.innerHTML = "";
+    console.log("Inserting Content")
+    generatorOutput.innerHTML = "<ul><li>" + input.join("</li><li>") + "</li></ul>";
+  console.groupEnd();
+}
+
+  // INPUTS
+  let genButton = document.getElementById("genButton");
+  genButton.addEventListener("click", function() {
+    pushToGeneratorOutput(itemCreateSet(5));
+  });
+
+  // OUTPUTS
+  let generatorOutput = document.getElementById("genHolder");
