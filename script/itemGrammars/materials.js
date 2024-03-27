fd77cb9a8e467be079cd0b1616a8310b139bb2fc
Jason Warren <contact.jwarren@gmail.com>
2024-03-27 16:50:24 +0000

    Commented out JS materials arrays

--- script/itemGrammars/materials.js
+++ script/itemGrammars/materials.js
@@ -1,12 +1,15 @@
 import * as util from "../utilities";
 
+/* Old JS Structure
 // Library
 const bone = ["bone", "horn", "ivory"];
 const metal = ["bronze", "copper", "gold", "iron", "mercury", "pewter", "silver", "steel"];
 const stone = ["flint", "granite", "obsidian"];
 const wood = ["ash", "elm", "mahogany", "oak", "willow"];
+*/
 
 // Categories
 const category = [bone, metal, stone, wood];
+
 
 export {category};
